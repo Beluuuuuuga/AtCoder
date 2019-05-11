f306ef4090361283de0a28d9f9d1f78ebf9bbb53
github Beluuuuuuga <inoue.koki15@gmail.com>
2019-05-11 16:39:08 +0900

    深さ優先探索の部分和pg作成

---
+++ lib/DFS_partial_sum.cpp
@@ -0,0 +1,35 @@
+//蟻本　p34参照
+#include <iostream>
+#include <stack>
+using namespace std;
+static const int MAX_N = 100;
+
+int a[MAX_N];
+int n, k;
+
+bool dfs(int i, int sum){
+  cout << i << " " << sum << endl;
+  if (i == n) return sum == k;
+  if (dfs(i + 1, sum)) return true;
+  if (dfs(i + 1, sum + a[i])) return true;
+  return false;
+}
+
+void solve(){
+  if (dfs(0,0)) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
+
+int main(){
+  int v;
+  cin >> n; // 4
+  // cin >> n;
+  for ( int i = 0; i < n; i++){
+    cin >> v;
+    a[i] = v; // a = [1,2,4,7]
+  }
+  cin >> k; // 13
+  solve();
+  // cout << a[0] <<a[1] << a[2] << endl;
+  return 0;
+}
