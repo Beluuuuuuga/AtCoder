cf465d9ba56a2d184fc51b7c87be325832fe3e3b
github Beluuuuuuga <inoue.koki15@gmail.com>
2019-05-13 22:57:49 +0900

    入力値を大文字から小文字に変更

--- diverta_2019/a.cpp
+++ diverta_2019/a.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-long long N, K;
+long long n,k,ans;
 
 int main(){
   int ans;
-  cin>>N>>K;
-  ans = N - K + 1;
+  cin>>n>>k;
+  ans=n-k+1;
   cout<<ans<<endl;
 }
--- diverta_2019/a.rb
+++ diverta_2019/a.rb
@@ -1,3 +1,3 @@
-N, K = gets.chop.split.map(&:to_i)
+n,k=gets.chop.split.map(&:to_i)
 
-puts N - K + 1
+puts n-k+1
