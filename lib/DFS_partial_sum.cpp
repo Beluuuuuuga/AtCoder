0faeac4bbf3d9d0b8ef7851dfd0b243ed4aba486
github Beluuuuuuga <inoue.koki15@gmail.com>
2019-05-19 13:03:05 +0900

    デバッグ用プログラムをコメントアウト

--- lib/DFS_partial_sum.cpp
+++ lib/DFS_partial_sum.cpp
@@ -8,7 +8,7 @@ int a[MAX_N];
 int n, k;
 
 bool dfs(int i, int sum){
-  cout << i << " " << sum << endl;
+  //cout << i << " " << sum << endl; //print debug
   if (i == n) return sum == k; //sum==kで条件式を書いていて、sum==kの時にtrueを返すようになっている
   if (dfs(i + 1, sum)) return true;
   if (dfs(i + 1, sum + a[i])) return true;
