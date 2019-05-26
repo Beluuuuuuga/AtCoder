d4ddfe12d763563560167cfe9d064edc5a32983d
github Beluuuuuuga <inoue.koki15@gmail.com>
2019-05-27 00:17:55 +0900

    sortのパターンを追加

--- lib/hash_sort.rb
+++ lib/hash_sort.rb
@@ -1,2 +1,3 @@
 bc=m.times.map{gets.split.map(&:to_i)}.sort_by{|b,c|c}  #valueで昇順
 bc=m.times.map{gets.split.map(&:to_i)}.sort_by{|b,c|-c} #valueで降順
+hash.sort_by { |i, a, b| [-b,a] } #複数の要素に対してhashの値でsortの条件を指定できる
