9b8b7d26d99582de77f453045008abe11b5d30c0
github Beluuuuuuga <inoue.koki15@gmail.com>
2019-07-08 23:01:38 +0900

    inputを更新

--- lib/input_ruby.rb
+++ lib/input_ruby.rb
@@ -7,7 +7,7 @@ N = gets.to_i #1 #=> 1
 # スペースで区切られた複数の整数
 a = gets.split.map(&:to_i) #1 2 3 #=> [1, 2, 3]
 
-# 3
+# 3_1
 # 縦に並んだ複数の整数。たまにある
 # N M
 # 1
@@ -15,6 +15,12 @@ a = gets.split.map(&:to_i) #1 2 3 #=> [1, 2, 3]
 # 2
 # 2
 a = N.times.map{gets.to_i}
+
+# 3_2
+# 関数化して使用
+def lscan; gets.split.map(&:to_i);end
+n, m = lscan
+a = n.times.map{lscan}
 
 # 4
 # 文字列。chompを付けないと改行文字がついてくる
@@ -31,3 +37,6 @@ a, b = N.times.map{gets.split.map(&:to_i)}.transpose
 # 6
 N = gets.to_i
 a = N.times.map{gets.split.map(&:to_i)}.transpose
+
+# 7 複数行読み込むとき
+s, t = readlines.map(&:chop)
